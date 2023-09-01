4e3408a520f121994a55ce2e8f03fe9f459e7a13
Michał Białkowski <michal@bialkowski.net.pl>
2023-09-01 14:13:35 +0200

    Update flickity.js

--- assets/js/custom/libs/triggers/flickity.js
+++ assets/js/custom/libs/triggers/flickity.js
@@ -8,26 +8,11 @@ $(window).on('load', function() {
 		autoPlay: false,
 		prevNextButtons: true,
         arrowShape: { 
-			x0: 15,
-			x1: 65, y1: 50,
-			x2: 65, y2: 40,
-			x3: 25
-		}
-	});
-    
-    $('.flickity--fullwidth').flickity({
-		cellAlign: 'left',
-		contain: true,
-		freeScroll: false,
-		wrapAround: true,
-		pageDots: true,
-		autoPlay: true,
-		prevNextButtons: false,
-        arrowShape: { 
-			x0: 15,
-			x1: 65, y1: 50,
-			x2: 65, y2: 40,
-			x3: 25
-		}
+            x0: 15,
+            x1: 65, y1: 50,
+            x2: 75, y2: 40,
+            x3: 35
+        }
+
 	});
 });
